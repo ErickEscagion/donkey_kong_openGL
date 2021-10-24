eb5dd79b5ce2f3902cd9de02739c59ba674dc6f4
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-10-24 20:34:03 -0300

    feat(core): removed some unused files and behaviours

--- Assets/Project/Core/Scripts/CoreController.cs
+++ Assets/Project/Core/Scripts/CoreController.cs
@@ -4,24 +4,6 @@ namespace Project.Core.Scripts
 {
     public class CoreController : MonoBehaviour
     {
-        #region Properties
-
-        public static CoreController Instance
-        {
-            get
-            {
-                if (_instance != null)
-                    return _instance;
-
-                var nav = Resources.Load<CoreController>("_coreController");
-                Instantiate(nav, Vector3.zero, Quaternion.identity);
-
-                return _instance;
-            }
-        }
-
-        #endregion
-
         #region Lifecycle
 
         private void Awake()
